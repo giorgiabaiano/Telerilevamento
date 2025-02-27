bdda443fec07001f23b01e9d8da6ad62f1f5858e
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-02-27 15:44:17 +0100

    Update first_script.R

--- Code/first_script.R
+++ Code/first_script.R
@@ -4,3 +4,10 @@
 anna <- 2+3 #assign an operation to an object
 chiara <- 4+6
 anna + chiara
+filippo <- c(0.2,0.4,0.6,0.8,0.9) #array
+luca <- c(100, 80, 60, 50, 10)
+plot (luca,filippo, pch=19, col="blue", cex=2, xlab="rubbish", ylab="biomass")
+# Installing packages
+# CRAN
+install.packages("terra")
+library (terrra)
