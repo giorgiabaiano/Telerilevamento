489a08dff6f18aabe6e86269e431f396f0e27ced
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-03-11 17:42:33 +0100

    Create 03_spectral_indices_TEMP.R

---
+++ Code/03_spectral_indices_TEMP.R
@@ -0,0 +1,36 @@
+#Code to calculate spectral indicies from satellite images
+
+library(terra)
+library(imageRy)
+library(viridis)
+
+im.list()
+
+# 1 = NIR
+# 2 =red
+# 3 = green
+
+mato1992= im.import( "matogrosso_l5_1992219_lrg.jpg")
+mato1992= flip(mato1992)
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato1992, r=2, g=3, b=1)
+
+#Import the 2006 image of the Mato Grosso area
+mato2006= im.import ( "matogrosso_ast_2006209_lrg.jpg")
+mato2006= flip(mato2006)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=2, g=3, b=1)
+
+im.multiframe (1,2)
+im.plotRGB(mato1992, r=2, g=3, b=1, title="MatoGrosso 1992")
+im.plotRGB(mato2006, r=2, g=3, b=1, title="MatoGrosso 2006")
+
+#Radiometric resolution
+plot(mato1992[[1]], col=inferno(100))
+plot(mato2006[[1]], col=inferno(100))
+
+#DVI
+#Tree: NIR=255, red=0, DVI= NIR-red=255
+#Stress tree: NIR=100, red=20, DIV=NIR-red=100-20=80
+
