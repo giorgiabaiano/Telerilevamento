20a742da316506017c11558187e74d04b7787f9f
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-03-13 15:47:15 +0100

    Update 03_spectral_indices_TEMP.R

--- Code/03_spectral_indices_TEMP.R
+++ Code/03_spectral_indices_TEMP.R
@@ -34,3 +34,58 @@ plot(mato2006[[1]], col=inferno(100))
 #Tree: NIR=255, red=0, DVI= NIR-red=255
 #Stress tree: NIR=100, red=20, DIV=NIR-red=100-20=80
 
+#Calculate DVI
+im.multiframe(1,2)
+plot(mato1992)
+plot(mato2006)
+
+#1 = NIR
+#2 =red
+
+dvi1992 = mato1992[[1]] - mato1992[[2]] #NIR - red
+plot(dvi1992)
+
+#DVI 8bit: range (0-255)
+#maximum: NIR - red = 255- 0 =255
+#minimum: NIR - red= 0 -22 = -255
+
+plot(dvi1992, col=inferno(100))
+
+#Calculate DVI for 2006:
+dvi2006 = mato2006[[1]] - mato2006[[2]] #NIR - red
+plot(dvi2006)
+
+plot(dvi2006, col=inferno(100))
+
+im.multiframe(1,2)
+plot(dvi1992, col=inferno(100))
+plot(dvi2006, col=inferno(100))
+
+#DVI 4bit: range (0-15)
+#maximum: NIR - red =15-0 = 15
+#minimum: NIR - red =0 - 15 = -15
+
+#NDVI 8bit: range (0-255)
+#maximum: (NIR - red) / (NIR + red) = (255 - 0) / (255 + 0) = 1
+#minimum: (NIR - red) / (NIR + red) = (255 - 0) / (255 + 0) = 1
+
+...
+
+#Function from imageRy
+dvi1992auto = im.dvi(mato1992, 1,2)
+plot(dvi1992auto)
+
+dvi2006auto = im.dvi(mato2006, 1,2)
+plot(dvi2006auto)
+
+ndvi1992auto = im.ndvi(mato1992, 1,2)
+plot(ndvi1992auto)
+
+ndvi2006auto = im.ndvi(mato2006, 1,2)
+plot(ndvi2006auto)
+
+im.multiframe ( 1,2)
+plot(ndvi1992)
+plot(ndvi1992auto)
+
+
