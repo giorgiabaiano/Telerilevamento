d82b8ade96011111c48cae0c52bf26c3c0ed1435
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-03-19 19:54:09 +0100

    Update 04_multitemporal analysis.R

--- Code/04_multitemporal analysis.R
+++ Code/04_multitemporal analysis.R
@@ -1,30 +1,63 @@
-# time series analysis in R
-
-library(terra)
 library(imageRy)
-library(ggridges) # for ridgeline plots
-library(ggplot2) # for ridgeline plots
-library(viridis) # for ridgeline plots
+library(terra)
+library(viridis)
 
 im.list()
 
-# Importing data
-EN01 <- im.import("EN_01.png")
-EN13 <- im.import("EN_13.png")
+EN_01 = im.import("EN_01.png")
+EN_01 = flip(EN_01)
+plot(EN_01)
 
-difEN = EN01[[1]] - EN13[[1]]
+EN_13 = im.import("EN_13.png")
+EN_13 = flip(EN_13)
+plot(EN_13)
 
-# Example 2: ice melt in Greenland
+# Exercise: plot the two images one beside the other
+im.multiframe(1,2)
+plot(EN_01)
+plot(EN_13)
 
-gr <- im.import("greenland")
-plot(gr[[1]])
-plot(gr[[4]])
+ENdif = EN_01[[1]] - EN_13[[1]]
+plot(ENdif)
+plot(ENdif, col=inferno(100))
 
-# Exercise: plot in a multiframe the first and last elements of gr
-par(mfrow=c(1,2))
-plot(gr[[1]])
-plot(gr[[4]])
 
-difgr = gr[[1]] - gr[[4]]
+
+# Greenland ice melt
+
+gr = im.import("greenland")
+
+im.multiframe(1,2)
+plot(gr[[1]], col=rocket(100))
+plot(gr[[4]], col=rocket(100))
+
+grdif = gr[[4]] - gr[[1]] # 2015 - 2000
+plot(grdif)
+# All the yellow parts are those in which there is a higher value in 2015
+
+# Exporting data
+setwd("/Users/ducciorocchini/Downloads")
+setwd("~/Desktop")
+# Windowds users: C://comp/Downloads
+# \
+# setwd("C://nome/Downloads")
+
+getwd()
+
+plot(gr)
+
+png("greenland_output.png")
+plot(gr)
 dev.off()
-plot(difgr)
+
+pdf("greenland_output.pdf")
+plot(gr)
+dev.off()
+
+pdf("difgreen.pdf")
+plot(grdif)
+dev.off()
+
+jpeg("difgreen.jpeg")
+plot(grdif)
+dev.off()
