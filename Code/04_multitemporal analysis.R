1cae36bc7f5bd9f462eb0b9da376f157bb70488e
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-03-19 19:28:07 +0100

    Update 04_multitemporal analysis.R

--- Code/04_multitemporal analysis.R
+++ Code/04_multitemporal analysis.R
@@ -1,1 +1,30 @@
+# time series analysis in R
 
+library(terra)
+library(imageRy)
+library(ggridges) # for ridgeline plots
+library(ggplot2) # for ridgeline plots
+library(viridis) # for ridgeline plots
+
+im.list()
+
+# Importing data
+EN01 <- im.import("EN_01.png")
+EN13 <- im.import("EN_13.png")
+
+difEN = EN01[[1]] - EN13[[1]]
+
+# Example 2: ice melt in Greenland
+
+gr <- im.import("greenland")
+plot(gr[[1]])
+plot(gr[[4]])
+
+# Exercise: plot in a multiframe the first and last elements of gr
+par(mfrow=c(1,2))
+plot(gr[[1]])
+plot(gr[[4]])
+
+difgr = gr[[1]] - gr[[4]]
+dev.off()
+plot(difgr)
