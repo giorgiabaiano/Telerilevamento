f5c6afaeafc6bd2c69b6bb328859e1c3bd4b0ffc
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-04-01 17:33:03 +0200

    Update 07_Classificatio.R

--- Code/07_Classificatio.R
+++ Code/07_Classificatio.R
@@ -1,7 +1,11 @@
 #R code for classifying images
 
+#install.packages("patchwork")
+
 library(terra)
 library(imageRy)
+library(ggplot2)
+library(patchwork)
 
 im.list()
 
@@ -33,5 +37,25 @@ prop2006 = f2006 / tot2006
 perc2006 = prop2006 * 100
 #human = 55%, forest = 45%
 
+#Creating dataframe
 
+class = c("Forest","Human")
+y1992 = c(83,17)
+y2006 = c(45,55)
+tabout = data.frame(class, y1992, y2006)
+
+p1= ggplot(tabout, aes(x=class, y=y1992, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100))
+
+p2 = ggplot(tabout, aes(x=class, y=y2006, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100))
+
+p1 + p2
+
+p0 = im.ggplot(mato1992)
+p00 = im.ggplot(mato2006)
+
+p0 + p00 + p1 + p2
 
