7e40cafe4a9aceafa3d6e1b0f69f953ade93f1cf
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-04-01 16:48:45 +0200

    Create 07_Classificatio.R

---
+++ Code/07_Classificatio.R
@@ -0,0 +1,37 @@
+#R code for classifying images
+
+library(terra)
+library(imageRy)
+
+im.list()
+
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 = flip(mato1992)
+plot(mato1992)
+
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+mato1992c = im.classify(mato1992, num_clusters=2)
+#class 1 = human
+#class 2 = forest
+
+mato2006c = im.classify(mato2006, num_clusters=2)
+#class 1 = forest
+#class 2 = human
+
+f1992 = freq(mato1992c)
+tot1992 = ncell(mato1992c)
+prop1992 = f1992 / tot1992
+perc1992 = prop1992 * 100
+#human =17%, forest =83%
+
+tot2006 = ncell(mato2006)
+f2006 = freq(mato2006c)
+prop2006 = f2006 / tot2006
+perc2006 = prop2006 * 100
+#human = 55%, forest = 45%
+
+
+
