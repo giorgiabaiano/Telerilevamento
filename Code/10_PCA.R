8968e6d39b89e1727ff06c8038cc8f57aac5a6bb
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-04-15 17:03:24 +0200

    Update 10_PCA.R

--- Code/10_PCA.R
+++ Code/10_PCA.R
@@ -1,1 +1,27 @@
+R code for principal component analysis
 
+library(imageRy)
+library(terra)
+im.list()
+
+sent= im.import("sentinel.png")
+sent = flip(sent)
+plot(sent)
+
+sent = c(sent[[1]],sent[[2]],sent[[3]])
+plot(sent)
+
+#NIR = band 1
+#red = band 2
+#green = band 3
+
+sentpca = im.pca(sent, n_samples=10000)
+tot = 72 + 59 + 6 
+#137
+# 72 : 137 = x : 100
+72 * 100/tot
+
+sdpc1 = focal(sentpca[[1]], w=c(3,3), fun="sd")
+plot(sdpc1)
+
+pairs(sent)
